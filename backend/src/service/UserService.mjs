1bf1ef8ca5c440d27449e367e6569775dc38319c
yraffic02 <yraffic02@gmail.com>
2024-08-10 16:02:12 -0300

    chore: criado user service

--- backend/src/service/UserService.mjs
+++ backend/src/service/UserService.mjs
@@ -1,8 +1,34 @@
-/* const UserService = {
-    insert: async (req) => {
-        const { name, email, password, username } = req.body;
+import db from '../models/index.mjs'
+import bcrypt from 'bcrypt'
+import { CrudHelper } from '../utils/crudHelper.mjs';
 
-    }
+const UserService = {
+   Create: async (req,_, crudHelper) =>{
+     try {
+        const { name, email, password, username} = req.body;
+
+        if(!name || !email || !password || !username){
+            throw new Error('Todos os dados são obrigatórios!')
+        }
+        
+        const hash = await bcrypt.hash(password.toString(), 10)
+
+        return await crudHelper({ 
+            name, 
+            email, 
+            password: hash, 
+            username
+        })
+     } catch (error) {
+        if(error.message.includes('Validation error')){
+            throw new Error('Usuário já existe!')
+        }
+        throw new Error(error)
+     }
+   }
 }
 
-export default UserService; */
+export default CrudHelper({
+    dbInstance: db.sequelize.models.Users,
+    service: UserService,
+});
