4a98fa0b5c3ac127ad9ad49de832e6c9d758127e
yraffic02 <yraffic02@gmail.com>
2024-08-10 15:55:46 -0300

    chore: correção de model

--- backend/src/models/index.mjs
+++ backend/src/models/index.mjs
@@ -1,34 +1,35 @@
 import fs from 'fs';
 import path from 'path';
 import { Sequelize } from 'sequelize';
-import process from 'process';
 import { fileURLToPath } from 'url';
 import config from '../configs/database.mjs';
 
-// __filename and __dirname are not available in ES modules, so we recreate them
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+/* const env = process.env.NODE_ENV || 'development'; */
 
 const db = {};
 const sequelize = new Sequelize(config);
 
-fs
+const modelFiles = fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-4) === '.mjs' &&
+      file.indexOf('.test.mjs') === -1
     );
-  })
-  .forEach(async (file) => {
-    const { default: model } = await import(path.join(__dirname, file));
-    db[model.name] = model(sequelize, Sequelize.DataTypes);
-  });
+});
+
+const modelImports = modelFiles.map(async (file) => {
+  const { default: model } = await import(path.join(__dirname, file));
+  db[model.name] = model(sequelize, Sequelize.DataTypes);
+});
+
+await Promise.all(modelImports);
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
