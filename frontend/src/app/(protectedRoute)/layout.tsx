fb9a3d2e645c81848f0445651b8c7519c512a430
yraffic02 <yraffic02@gmail.com>
2024-08-13 11:48:47 -0300

    feat: criado layout de das rotas protegidas

---
+++ frontend/src/app/(protectedRoute)/layout.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
