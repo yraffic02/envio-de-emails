59c46d7df63fbcf12707206b2d3c9f128fa7d0a0
yraffic02 <yraffic02@gmail.com>
2024-08-14 09:14:57 -0300

    chor: add dropdown

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,42 +2,21 @@
 import Link from "next/link";
 import {
     Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarTrigger
 } from "../ui/menubar";
 import { AtSign } from "lucide-react";
+import { Dropdown } from "../DropdownMenu/Dropdown";
 
 export default function Navbar() {
     return (
-        <Menubar className="bg-slate-950 justify-between p-4">
-            <Link href='/' className="flex text-white gap-2">
+        <Menubar className="bg-slate-950 justify-between p-6 text-white">
+            <Link href='/' className="flex gap-2">
                 <h1 className="font-bold cursor-pointer">
                     Envio Email
                 </h1>
                 <AtSign />
             </Link>
             <div />
-            <MenubarMenu>
-                <MenubarTrigger
-                    className="text-white"
-                >
-                    Fazer login
-                </MenubarTrigger>
-                <MenubarContent>
-                    <Link href='/auth/login'>
-                        <MenubarItem>
-                            Entra
-                        </MenubarItem>
-                    </Link>
-                    <Link href='/auth/register'>
-                        <MenubarItem>
-                            Cadastra-se
-                        </MenubarItem>
-                    </Link>
-                </MenubarContent>
-            </MenubarMenu>
+            <Dropdown />
         </Menubar>
     )
 }
