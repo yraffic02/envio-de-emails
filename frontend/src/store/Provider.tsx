13abf86946a79ceb6b33051c2061c50695af40d4
yraffic02 <yraffic02@gmail.com>
2024-09-01 23:44:03 -0300

    chore: add provider

---
+++ frontend/src/store/Provider.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { ReactNode } from 'react'
+
+export function Providers({ children }: { children: ReactNode }) {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
