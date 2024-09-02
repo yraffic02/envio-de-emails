6c85a4d6bc4028a592b44b2671c619c684bbc59f
yraffic02 <yraffic02@gmail.com>
2024-09-01 23:46:59 -0300

    chore: add constantes

--- frontend/src/configs/constants.ts
+++ frontend/src/configs/constants.ts
@@ -1,5 +1,7 @@
-import { AccordionProps } from "./interfaces";
-
+import { AccordionProps, IActionObj, LinkDropdown } from "./interfaces";
+import { FormLogin } from "@/components/auth/FormLogin";
+import { FormTotp } from "@/components/auth/FormTotp";
+import { FormUser } from "@/components/User/FormUser/FormUser";
 
 export const accordionConstants: AccordionProps = {
     accordionObject: [
@@ -16,3 +18,37 @@ export const accordionConstants: AccordionProps = {
 
 
 export const publicRoutes = ['/', '/auth/login', '/auth/register', '/auth/totp'];
+
+export const linkDropdown: LinkDropdown[] = [
+    {
+        link:  '#',
+        name: 'Perfil'
+    },
+    {
+        link:  '/config',
+        name: 'Configuração'
+    },
+]
+
+export type Action = 'login' | 'register' | 'totp';
+
+export const actionMap: Record<Action, IActionObj> = {
+    login: {
+      component: FormLogin,
+      text: "Não tem um cadastro?",
+      linkText: "Criar uma conta",
+      linkHref: "/auth/register",
+    },
+    register: {
+      component: FormUser,
+      text: "Já tem cadastro?",
+      linkText: "Faça login",
+      linkHref: "/auth/login",
+    },
+    totp: {
+      component: FormTotp,
+      text: null,
+      linkText: null,
+      linkHref: null,
+    },
+};
