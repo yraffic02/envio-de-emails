e0980bcc65af48c33953bc544cf3d4cf1f1e5d74
yraffic02 <yraffic02@gmail.com>
2024-09-01 23:46:44 -0300

    chore: criado arquivo interface

--- frontend/src/configs/interfaces.ts
+++ frontend/src/configs/interfaces.ts
@@ -1,6 +1,37 @@
+import {ComponentType, ReactNode} from "react";
+import { Action } from "./constants";
+
 export interface AccordionProps{
     accordionObject: {
         title: string,
         description: string,
     }[];
+}
+
+export interface LinkDropdown{
+    link: string,
+    name: string
+}
+
+export interface IModal {
+    isOpen: boolean,
+    onClose: () => void,
+    children: ReactNode
+}
+
+export interface IFormComponentProps {
+    children: ReactNode;
+    onSubmit: () => void;
+    buttonName: string;
+}
+
+export interface IAuthParams {
+    action: Action;
+}
+
+export interface IActionObj {
+    component: ComponentType;
+    text: string | null;
+    linkText: string | null;
+    linkHref: string | null;
 }
