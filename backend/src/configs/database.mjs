38c0157c3a57413da55d2c4da9ee6c93046e6fc7
yraffic02 <yraffic02@gmail.com>
2024-09-03 11:28:47 -0300

    chore: config para db em prod

--- backend/src/configs/database.mjs
+++ backend/src/configs/database.mjs
@@ -3,12 +3,28 @@ import process from 'process';
 
 dotenv.config();
 
-export default {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-  host: process.env.MYSQL_HOST,
-  dialect: 'mysql',
-  logging: false,
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DB_DIALECT_DEV || 'mysql',
+    logging: true,
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DB_DIALECT_PROD || 'postgres',
+    logging: true,
+  },
 };
+console.log(env);
+
+export default config[env];
